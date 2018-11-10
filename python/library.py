a165f689409cb656c2e7b14699be5f3fffe17199
pieteradejong <pieter.a.dejong@gmail.com>
2018-11-10 13:21:45 -0500

    expand

--- python/library.py
+++ python/library.py
@@ -16,3 +16,31 @@ else:
     print "It does not match"
 
 
+'''
+plots: line plot 
+
+'''
+
+import numpy as np
+import pylab as pl
+
+x = [1, 2, 3, 4, 5]
+y = [1, 4, 9, 16, 25]
+
+pl.plot(x, y)
+pl.show()
+
+
+'''
+plots: scatter plot
+'''
+
+import numpy as np
+import pylab as pl
+
+x = [1, 2, 3, 4, 5]
+y = [1, 4, 9, 16, 25]
+
+pl.plot(x, y)
+pl.show()
+
